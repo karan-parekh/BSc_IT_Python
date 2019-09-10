a14a15d2aeeb525d33167f1986c8143bc63dd690
karan-parekh <karanparekh501@gmail.com>
2019-09-10 16:58:03 +0530

    minor change

--- pracs/unit_4/prac_4a_common_element.py
+++ pracs/unit_4/prac_4a_common_element.py
@@ -1,8 +1,8 @@
 # PYTHON PROGRAM TO RETURN TRUE IF TEHRE IS AT LEAST ONE COMMON ELEMENT IN TWO LISTS
 
 # initializing two lists
-list_1 = [44, 44, 22, 71, 67, 89, 20, 99]
-list_2 = [93, 68, 17, 49, 20, 12, 57, 10]
+num_1 = [44, 44, 22, 71, 67, 89, 20, 99]
+num_2 = [93, 68, 17, 49, 20, 12, 57, 10]
 
 # function to return True if there exists a common element in the lists else False
 def is_common_element(l1, l2):
@@ -15,7 +15,7 @@ def is_common_element(l1, l2):
 	return False
 
 # output
-if is_common_element(list_1, list_2):
+if is_common_element(num_1, num_2):
 	print("There exists at least one common element in the lists")
 else:
 	print("No common element found")
